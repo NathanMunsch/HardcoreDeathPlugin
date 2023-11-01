0dfd65712110b9353782c6efe2efff5a98f09001
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-01 14:55:55 +0100

    Initial commit

---
+++ src/main/java/us/developers/hardcoredeathplugin/HardcoreDeathPlugin.java
@@ -0,0 +1,17 @@
+package us.developers.hardcoredeathplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HardcoreDeathPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

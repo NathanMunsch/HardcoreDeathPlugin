0883d71b9808bd652a37f2beeffedbd19606e73c
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-04 14:45:20 +0100

    feat: Setup onEnable and onDisable methods in the main class

--- src/main/java/us/developers/hardcoredeathplugin/HardcoreDeathPlugin.java
+++ src/main/java/us/developers/hardcoredeathplugin/HardcoreDeathPlugin.java
@@ -1,17 +1,49 @@
 package us.developers.hardcoredeathplugin;
 
+import org.bukkit.Bukkit;
+import us.developers.hardcoredeathplugin.database.Database;
+import us.developers.hardcoredeathplugin.events.PlayerDiedEvent;
+import us.developers.hardcoredeathplugin.events.PlayerJoinedEvent;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class HardcoreDeathPlugin extends JavaPlugin {
+import java.sql.SQLException;
+
+
+public final class HardcoreDeathPlugin extends JavaPlugin implements Listener {
+
+    public static Database db;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        if (getServer().isHardcore()) {
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+            try {
+                db = new Database(getDataFolder().getAbsolutePath() + "/HardcoreDeathPlugin.db");
+            } catch (SQLException e) {
+                System.out.println("Failed to connect to the database : " + e.getMessage());
+                Bukkit.getPluginManager().disablePlugin(this);
+            }
 
+            getServer().getPluginManager().registerEvents(new PlayerDiedEvent(), this);
+            getServer().getPluginManager().registerEvents(new PlayerJoinedEvent(), this);
+
+            System.out.println("The HardcoreDeathPlugin has started");
+        }
+        else {
+            System.out.println("The HardcoreDeathPlugin hasn't started because the hardcore mode is not enabled");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        try {
+            db.closeConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

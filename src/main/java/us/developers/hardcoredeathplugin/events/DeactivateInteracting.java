a7399c69df06a12c477879402aec104acd1393b7
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-05 16:58:34 +0100

    feat: Deactivate all interactions if the player is in repay mode

---
+++ src/main/java/us/developers/hardcoredeathplugin/events/DeactivateInteracting.java
@@ -0,0 +1,15 @@
+package us.developers.hardcoredeathplugin.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import us.developers.hardcoredeathplugin.HardcoreDeathPlugin;
+
+public class DeactivateInteracting implements Listener {
+    @EventHandler
+    public void whenInteracting(PlayerInteractEvent playerInteractEvent) {
+        if (HardcoreDeathPlugin.db.isInRepayMode(playerInteractEvent.getPlayer())) {
+            playerInteractEvent.setCancelled(true);
+        }
+    }
+}

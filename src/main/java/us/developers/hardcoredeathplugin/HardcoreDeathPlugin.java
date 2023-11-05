72cc2f7bb5e4b5f3c80309e7ace92a3227cf4af4
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-05 17:01:08 +0100

    feat: Add log leveling in main class

--- src/main/java/us/developers/hardcoredeathplugin/HardcoreDeathPlugin.java
+++ src/main/java/us/developers/hardcoredeathplugin/HardcoreDeathPlugin.java
@@ -8,6 +8,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLException;
+import java.util.logging.Level;
 
 
 public final class HardcoreDeathPlugin extends JavaPlugin implements Listener {
@@ -18,28 +19,29 @@ public final class HardcoreDeathPlugin extends JavaPlugin implements Listener {
     public void onEnable() {
         if (getServer().isHardcore()) {
             getServer().getWorlds().forEach(world -> world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true));
-            System.out.println("Due to the way the plugin HardcoreDeathPlugin works, " +
-                    "the gamerule doImmediateRespawn has been activated to allow the plugin to work correctly");
+            getLogger().log(Level.WARNING, "Due to the way the plugin works, the gamerule doImmediateRespawn " +
+                    "has been activated to allow the plugin to work correctly");
 
             if (!getDataFolder().exists()) {
                 getDataFolder().mkdirs();
             }
 
             try {
-                db = new Database(getDataFolder().getAbsolutePath() + "/HardcoreDeathPlugin.db");
+                db = new Database( getDataFolder().getAbsolutePath() + "/HardcoreDeathPlugin.db");
             } catch (SQLException e) {
-                System.out.println("Failed to connect to the database : " + e.getMessage());
+                getLogger().log(Level.SEVERE, "(Plugin disabled) Failed to connect to the " +
+                        "database : " + e.getMessage());
                 Bukkit.getPluginManager().disablePlugin(this);
             }
 
+            getServer().getPluginManager().registerEvents(new DeactivateInteracting(), this);
+            getServer().getPluginManager().registerEvents(new OnConnectionEvent(), this);
             getServer().getPluginManager().registerEvents(new PlayerPickedUpItem(), this);
-            getServer().getPluginManager().registerEvents(new PreventChestOpening(), this);
-            getServer().getPluginManager().registerEvents(new RegisterPlacedBlock(), this);
 
-            System.out.println("The HardcoreDeathPlugin has correctly started");
+            getLogger().log(Level.INFO, "The plugin has correctly started");
         }
         else {
-            System.out.println("The HardcoreDeathPlugin hasn't started because the hardcore mode is not enabled");
+            getLogger().log(Level.WARNING, "The plugin hasn't started because the hardcore mode is not enabled");
             Bukkit.getPluginManager().disablePlugin(this);
         }
     }

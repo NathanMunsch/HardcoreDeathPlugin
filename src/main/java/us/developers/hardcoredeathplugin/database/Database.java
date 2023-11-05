b5016ba02713e4ed3ac5ca944182f3ebf3cee25b
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-05 16:59:56 +0100

    feat: Add method initializeQuests and delete auto increment from quests table

--- src/main/java/us/developers/hardcoredeathplugin/database/Database.java
+++ src/main/java/us/developers/hardcoredeathplugin/database/Database.java
@@ -1,7 +1,6 @@
 package us.developers.hardcoredeathplugin.database;
 
 import org.bukkit.entity.Player;
-
 import java.sql.*;
 
 public class Database {
@@ -9,28 +8,81 @@ public class Database {
 
     public Database(String path) throws SQLException {
         connection = DriverManager.getConnection("jdbc:sqlite:" + path);
-        Statement statement = connection.createStatement();
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS players (
-            uuid TEXT PRIMARY KEY,
-            username TEXT NOT NULL,
-            repayMode INTEGER NOT NULL,
-            deaths INTEGRER NOT NULL DEFAULT 0)
-            """);
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS quests (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT UNIQUE NOT NULL,
-            objective INTEGER NOT NULL)
-            """);
-        statement.execute("""
-            CREATE TABLE IF NOT EXISTS players_quests (
-            player_uuid TEXT,
-            quests_id INTEGER,
-            PRIMARY KEY (player_uuid, quests_id),
-            FOREIGN KEY (player_uuid) REFERENCES players(uuid),
-            FOREIGN KEY (quests_id) REFERENCES quests(id))
-            """);
+        createTables();
+        initializeQuests();
+    }
+
+    private void createTables() {
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            statement.execute("""
+                CREATE TABLE IF NOT EXISTS players (
+                uuid TEXT PRIMARY KEY,
+                repayMode INTEGER NOT NULL DEFAULT 0,
+                deaths INTEGRER NOT NULL DEFAULT 0)
+                """);
+            statement.execute("""
+                CREATE TABLE IF NOT EXISTS quests (
+                id INTEGER PRIMARY KEY,
+                name TEXT UNIQUE NOT NULL,
+                objective INTEGER NOT NULL)
+                """);
+            statement.execute("""
+                CREATE TABLE IF NOT EXISTS players_quests (
+                player_uuid TEXT,
+                quests_id INTEGER,
+                PRIMARY KEY (player_uuid, quests_id),
+                FOREIGN KEY (player_uuid) REFERENCES players(uuid),
+                FOREIGN KEY (quests_id) REFERENCES quests(id))
+                """);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void initializeQuests() {
+        String[][] quests = new String[][] {
+                {"Treasure hunt", "1"},
+                {"Kill a wither", "1"},
+                {"Make an exchange with an NPC", "1"},
+                {"Craft an anvil", "1"},
+                {"Catch 5 fish", "5"},
+                {"Kill 10 zombies", "10"},
+                {"Melt 10 gold bars", "10"},
+                {"Craft a diamond pickaxe", "1"},
+                {"Activate a nether portal", "1"},
+                {"Breed two cows", "2"},
+                {"Ride a minecart", "1"},
+                {"Craft an enchanting table", "1"},
+                {"Mine an ancient debris", "1"}
+        };
+
+        for (int i = 0; i < quests.length; i++) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT OR IGNORE INTO " +
+                    "quests(id, name, objective) VALUES (?, ?, ?)")) {
+                preparedStatement.setInt(1, getLastIdFromQuestsTable() + 1);
+                preparedStatement.setString(2, quests[i][0]);
+                preparedStatement.setInt(3, Integer.parseInt(quests[i][1]));
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private int getLastIdFromQuestsTable() {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT MAX(id) AS 'lastId' FROM quests")) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int lastId = resultSet.getInt("lastId");
+                resultSet.close();
+                return lastId;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
     }
 
     public void closeConnection() {
@@ -44,10 +96,8 @@ public class Database {
     }
 
     public void addPlayer(Player player) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO players(uuid, username, repayMode) VALUES (?, ?, ?)")) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT OR IGNORE INTO players(uuid) VALUES (?)")) {
             preparedStatement.setString(1, player.getUniqueId().toString());
-            preparedStatement.setString(2, player.getDisplayName());
-            preparedStatement.setInt(3, 1);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);

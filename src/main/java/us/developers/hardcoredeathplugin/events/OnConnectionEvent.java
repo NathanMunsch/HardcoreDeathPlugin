54fdfa5c52c6523b3d80ef083cc396d0af2de71b
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-05 17:02:59 +0100

    feat: Add player in database when login in

---
+++ src/main/java/us/developers/hardcoredeathplugin/events/OnConnectionEvent.java
@@ -0,0 +1,13 @@
+package us.developers.hardcoredeathplugin.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import us.developers.hardcoredeathplugin.HardcoreDeathPlugin;
+
+public class OnConnectionEvent implements Listener {
+    @EventHandler
+    public void onConnection(PlayerLoginEvent playerLoginEvent) {
+        HardcoreDeathPlugin.db.addPlayer(playerLoginEvent.getPlayer());
+    }
+}

9717846767aab33415c77b6a4417ea81a508984e
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-10 18:20:13 +0100

    feat: Create event for quests that require braking a block

--- src/main/java/us/developers/hardcoredeathplugin/events/quests/OnMinedBlock.java
+++ src/main/java/us/developers/hardcoredeathplugin/events/quests/OnMinedBlock.java
@@ -1,6 +1,7 @@
-package us.developers.hardcoredeathplugin.events;
+package us.developers.hardcoredeathplugin.events.quests;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -9,9 +10,12 @@ import us.developers.hardcoredeathplugin.HardcoreDeathPlugin;
 public class OnMinedBlock implements Listener {
     @EventHandler
     public void playerMinedBlock(BlockBreakEvent blockBreakEvent) {
-        if (HardcoreDeathPlugin.db.isInRepayMode(blockBreakEvent.getPlayer())) {
+        Player player = blockBreakEvent.getPlayer();
+        if (HardcoreDeathPlugin.db.getRepayMode(player)) {
+
+            // Quest mine an ancien debris
             if (blockBreakEvent.getBlock().getType() == Material.ANCIENT_DEBRIS) {
-                // fonction a appeler en bdd
+                HardcoreDeathPlugin.db.addProgression(player, 14);
             }
         }
     }

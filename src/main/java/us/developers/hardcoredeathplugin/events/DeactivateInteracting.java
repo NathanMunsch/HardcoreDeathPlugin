e8cab0d60555669f7bf8e63f3de56b04df223020
NathanMunsch <nathan2003munsch@gmail.com>
2023-11-12 12:58:34 +0100

    feat: deactivate interacting but just for certain blocks

--- src/main/java/us/developers/hardcoredeathplugin/events/DeactivateInteracting.java
+++ src/main/java/us/developers/hardcoredeathplugin/events/DeactivateInteracting.java
@@ -1,15 +1,28 @@
 package us.developers.hardcoredeathplugin.events;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import us.developers.hardcoredeathplugin.HardcoreDeathPlugin;
 
+import java.util.ArrayList;
+
 public class DeactivateInteracting implements Listener {
     @EventHandler
     public void whenInteracting(PlayerInteractEvent playerInteractEvent) {
+        // List of blocks with deactivate interacting
+        ArrayList<Material> materials = new ArrayList<>();
+        materials.add(Material.CHEST);
+        materials.add(Material.BARREL);
+        materials.add(Material.SHULKER_BOX);
+
         if (HardcoreDeathPlugin.db.getRepayMode(playerInteractEvent.getPlayer())) {
-            playerInteractEvent.setCancelled(true);
+            if (playerInteractEvent.hasBlock()) {
+                if (materials.contains(playerInteractEvent.getClickedBlock().getType())) {
+                    playerInteractEvent.setCancelled(true);
+                }
+            }
         }
     }
 }
